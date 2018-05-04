489164cd45999eceb6e601a455f7611ced40e94a
travisholt92 <Travisholt92@gmail.com>
2018-05-04 14:19:43 -0400

    Fix building 64bit and fix DT2W for K20 Plus

--- drivers/input/touchscreen/lge/touch_core.c
+++ drivers/input/touchscreen/lge/touch_core.c
@@ -618,7 +618,6 @@ static void touch_send_uevent(struct touch_core_data *ts, int type)
 		input_report_key(ts->input, KEY_WAKEUP, 0);
 		TOUCH_I("Simulate power button release");
 		input_sync(ts->input);
-		}
 	}
 }
 
