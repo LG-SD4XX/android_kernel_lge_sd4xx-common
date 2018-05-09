5f45824e0e6685ba217337a42fa8f94ef0908886
deadman96385 <seanhoyt963@gmail.com>
2018-05-08 21:25:35 -0500

    Add LGE NFC Drivers

--- drivers/nfc/nq-nci.h
+++ drivers/nfc/nq-nci.h
@@ -10,8 +10,8 @@
  * GNU General Public License for more details.
  */
 
-#ifndef __NQ_NCI_H
-#define __NQ_NCI_H
+#ifndef __NFC_NCI_H
+#define __NFC_NCI_H
 
 #include <linux/i2c.h>
 #include <linux/types.h>
@@ -31,6 +31,7 @@
 #define NFCC_INITIAL_CORE_RESET_NTF	_IOW(0xE9, 0x10, unsigned int)
 
 #define NFC_RX_BUFFER_CNT_START		(0x0)
+
 #define PAYLOAD_HEADER_LENGTH		(0x3)
 #define PAYLOAD_LENGTH_MAX		(256)
 #define BYTE				(0x8)
